4e2384e95ac3476f4f386e32909856f480a9988a
penguinflipper <astressertsa@outlook.com>
2025-11-19 10:50:56 +0000

    Fixed BaseStorageTest.storeFile test

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/EmbeddedDatabase.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/storage/EmbeddedDatabase.java
@@ -84,7 +84,7 @@ public class EmbeddedDatabase {
 			try {
 				em.getTransaction().begin();
 				
-				// If the object is an EntityArchive, merge its workspace first
+				// If the object is an EntityArchive, merge its workkspace first
 				// to avoid foreign key constraint issues
 				if (obj instanceof EntityArchive) {
 					EntityArchive a = (EntityArchive) obj;
