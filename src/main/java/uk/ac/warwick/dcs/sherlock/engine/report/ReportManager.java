4863139ec55da33f7409dff7d2fd5886b174a281
Henry Gao <Henry.Gao@warwick.ac.uk>
2026-02-22 16:51:20 +0000

    Reverted submission score change (always unique id)

--- src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportManager.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/engine/report/ReportManager.java
@@ -74,8 +74,7 @@ public class ReportManager implements IReportManager<SubmissionMatchGroup, Submi
 			if (resultFile.getFile() == null) return;
 			ISubmission submission = resultFile.getFile().getSubmission();
 			if (submission == null) return;
-			// Use max score across all files in the submission
-			this.submissionScores.merge(submission.getId(), resultFile.getOverallScore(), Math::max);
+			this.submissionScores.put(submission.getId(), resultFile.getOverallScore());
 		});
 
 		FillSubmissionFileMap();
