d3067d098c42b848398276b2cf2a374699632554
Joshiling <mail@joshuarawlings.com>
2026-04-06 21:34:53 +0100

    Added AbstractMatch and made all RawResults use classes inhereted from this

--- src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/NGramMatch.java
+++ src/main/java/uk/ac/warwick/dcs/sherlock/module/model/base/detection/NGramMatch.java
@@ -2,6 +2,7 @@ package uk.ac.warwick.dcs.sherlock.module.model.base.detection;
 
 import uk.ac.warwick.dcs.sherlock.api.component.ISourceFile;
 import uk.ac.warwick.dcs.sherlock.api.util.Tuple;
+import uk.ac.warwick.dcs.sherlock.module.model.base.detection.AbstractMatch;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,19 +19,7 @@ import java.util.Objects;
  *     A pointer to the checked file (File2).
  * </p>
  */
-public class NGramMatch implements Serializable {
-    /**
-     * The line positions of both blocks.
-     */
-    public ArrayList<Tuple<Integer, Integer>> lines;    // array list used for type safety of generics
-    /**
-     * The similarity between the section of both files.
-     */
-    public float similarity;
-    /**
-     * The two files with matching sections.
-     */
-    public ISourceFile[] files;
+public class NGramMatch extends AbstractMatch {
 
     /**
      * Constructor, stores all inputted data in the container object.
@@ -43,46 +32,6 @@ public class NGramMatch implements Serializable {
      * @param file2 The second file.
      */
     NGramMatch(int refStart, int refEnd, int checkStart, int checkEnd, float similarity, ISourceFile file1, ISourceFile file2) {
-        // init the array list
-        lines = new ArrayList<>();
-        // fill with line positions
-        lines.add(new Tuple<>(refStart, refEnd));
-        lines.add(new Tuple<>(checkStart, checkEnd));
-
-        this.similarity = similarity;
-        // init the array to a pair
-        files = new ISourceFile[2];
-        // store the file objects (pointers, the same pointers should be used globally)
-        files[0] = file1;
-        files[1] = file2;
-    }
-
-    /**
-     * Checks if two matches reference the same line ranges.
-     * Two matches are equal if they span the same lines in both files,
-     * regardless of similarity score or file references.
-     *
-     * @param obj the object to compare with
-     * @return true if both matches have identical line ranges, false otherwise
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (!(obj instanceof NGramMatch)) return false;
-        NGramMatch other = (NGramMatch) obj;
-        
-        return this.lines.get(0).equals(other.lines.get(0))
-            && this.lines.get(1).equals(other.lines.get(1));
-    }
-
-    /**
-     * Returns hash code based on line ranges.
-     * Consistent with equals() — only considers line positions, not similarity or files.
-     *
-     * @return hash code value for this match
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(lines.get(0), lines.get(1));
+        super(refStart, refEnd, checkStart, checkEnd, similarity, file1, file2);
     }
 }
